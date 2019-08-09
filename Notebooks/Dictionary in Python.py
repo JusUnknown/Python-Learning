2d6bb4e62301fda6acc3f76e412eb0cdde9bd0e1
Jus Twonn <22135504+JusUnknown@users.noreply.github.com>
2019-08-09 08:43:44 -0400

    jupyter to python
    Work following tuts

---
+++ Notebooks/Dictionary in Python.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_dict = {'key1':'value1','key2':'value2'}
+
+
+# In[2]:
+
+
+my_dict
+
+
+# In[3]:
+
+
+my_dict['key1']
+
+
+# In[4]:
+
+
+prices_lookup = {'apple':2.99,'oranges':1.99,'milk':5.80}
+
+
+# In[5]:
+
+
+prices_lookup['apple']
+
+
+# In[6]:
+
+
+d = {'k1':123,'k2':[0,1,2], 'k3':{'insideKey':100
+}}
+
+
+# In[7]:
+
+
+d['k2']
+
+
+# In[8]:
+
+
+d['k3']
+
+
+# In[9]:
+
+
+d['k3']['insideKey']
+
+
+# In[10]:
+
+
+d['k2'][2]
+
+
+# In[11]:
+
+
+d={'key1':['a','b','c']}
+
+
+# In[12]:
+
+
+d
+
+
+# In[13]:
+
+
+mylist = d['key1']
+
+
+# In[14]:
+
+
+mylist
+
+
+# In[15]:
+
+
+letter = mylist[2]
+
+
+# In[16]:
+
+
+letter
+
+
+# In[17]:
+
+
+letter.upper()
+
+
+# In[18]:
+
+
+d['key1'][2].upper()
+
+
+# In[19]:
+
+
+d = {'k1':100,'k2':200}
+
+
+# In[20]:
+
+
+d
+
+
+# In[21]:
+
+
+d['k3'] = 300
+
+
+# In[22]:
+
+
+d
+
+
+# In[23]:
+
+
+d['k1'] =  'NEW VALUE'
+
+
+# In[24]:
+
+
+d
+
+
+# In[25]:
+
+
+d = {'k1': 100, 'k2': 200, 'k3': 300}
+
+
+# In[26]:
+
+
+d.keys
+
+
+# In[27]:
+
+
+d.keys()
+
+
+# In[28]:
+
+
+d.values()
+
+
+# In[29]:
+
+
+d.items()
+
+
+# In[30]:
+
+
+pwd
+
+
+# In[ ]:
+
+
+
+
