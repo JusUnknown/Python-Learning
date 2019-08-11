97a6bb75ea8d4b3df29fc99d70a7296451e6166c
Jus Twonn <22135504+jusunknown@users.noreply.github.com>
2019-08-11 08:45:46 -0400

    Loops/if else/ for / translate.
    1. Guessing Game
    2. Loops practice
    3. Vowel translate

---
+++ loops.py
@@ -0,0 +1,27 @@
+#raise to power and for loops
+
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3, 4))
+
+##Add prompt to let user input own numberss
+
+number_grid = [
+    [1, 2, 3, ],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+for row in number_grid:
+    for col in row:
+        print(col)
+
+
+
+
